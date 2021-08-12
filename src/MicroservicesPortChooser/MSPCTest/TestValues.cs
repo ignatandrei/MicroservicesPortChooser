6deeffe8b3b35dfd585cb9cc1ef68365be933240
ignatandrei <ignatandrei@yahoo.com>
2021-08-12 03:41:53 +0300

    first iterations

---
+++ src/MicroservicesPortChooser/MSPCTest/TestValues.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MSPCTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        public void TestSamePort()
+        {
+
+        }
+    }
+}
---
+++ src/MicroservicesPortChooser/MicroservicesPortChooserBL/MSPC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MicroservicesPortChooserBL
+{
+    public class MSPC
+    {
+        public UInt16 Port(string name)
+        {
+            int hash =Math.Abs( name.GetHashCode());
+            if (hash < UInt16.MaxValue)
+                return (UInt16)hash;
+
+            var remainder = hash % UInt16.MaxValue;
+            return (UInt16)remainder;
+        }
+    }
+}
