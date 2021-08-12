220b1c1446acd3a9e9269d20fb23d8d2c8b9ce40
ignatandrei <ignatandrei@yahoo.com>
2021-08-12 04:42:29 +0300

    added web

---
+++ src/MicroservicesPortChooser/MicroservicesPortChooserWeb/Controllers/PortChooserController.cs
@@ -0,0 +1,35 @@
+﻿using MicroservicesPortChooserBL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroservicesPortChooserWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PortChooserController : ControllerBase
+    {
+
+        private readonly ILogger<PortChooserController> _logger;
+
+        public PortChooserController(ILogger<PortChooserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<UInt16> GetDeterministicPortFrom([FromServices] MSPC mspc, string name)
+        {
+            return mspc.GetDeterministicPort(name);
+        }
+        [HttpGet("{name}")]
+        public ActionResult<UInt16> GetNonDeterministicPortFrom([FromServices] MSPC mspc, string name)
+        {
+            return mspc.GetNonDeterministicPort(name);
+        }
+
+    }
+}
