4f1c90904efe0adc7fa46122dc561a3df567c6d9
ignatandrei <ignatandrei@yahoo.com>
2021-08-12 20:15:47 +0300

    more explanation

--- src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
+++ src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
@@ -9,22 +9,28 @@ import { MPCv1Service } from '../services/mpcv1.service';
 export class MPCv1Component implements OnInit {
 
   appName: string = '';
+  pcName: string=  'localhost';
+  fullName: string = '';
   constructor(private mpcv1Service: MPCv1Service) { }
 
   ngOnInit(): void {
 
   }
-
+  constructFullName(port:string): string{
+    this.fullName = 'http://'+ (this.pcName ||'localhost') + ':' + port;
+    return this.fullName;
+  } 
+  
   onDeterministic(): void {
 
      this.mpcv1Service.deterministic(this.appName).subscribe(
-       v=> window.alert(v)
+       v=> window.alert(this.constructFullName(v))
      );
   }
 
   onNonDeterministic(): void {
     this.mpcv1Service.nonDeterministic(this.appName).subscribe(
-      v=> window.alert(v)
+      v=> window.alert(this.constructFullName(v))
     );
   }
 
