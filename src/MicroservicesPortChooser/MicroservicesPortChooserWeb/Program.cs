1095d1943b0fcec68b0be8a33c43b82507757bcc
ignatandrei <ignatandrei@yahoo.com>
2021-08-15 09:59:41 +0300

    start port chooser

--- src/MicroservicesPortChooser/MicroservicesPortChooserWeb/Program.cs
+++ src/MicroservicesPortChooser/MicroservicesPortChooserWeb/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MicroservicesPortChooserWeb
@@ -14,6 +17,26 @@ namespace MicroservicesPortChooserWeb
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static async Task<int> GetPort(string url)
+        {
+            HttpClient httpClient=null;
+            try{
+            //https://microservicesportchooser.azurewebsites.net/api/v1/PortChooser/GetDeterministicPortFrom/{name}
+            httpClient =new HttpClient();
+            
+            var port = await httpClient.GetStringAsync(url);            
+            return int.Parse(port);
+            // Console.WriteLine(port);
+            }
+            catch  {
+                return -1;
+            }
+            finally{
+                httpClient?.Dispose();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
