0d729758ceec9ab8eaf1ba03df6e88488bbdaa1a
ignatandrei <ignatandrei@yahoo.com>
2021-08-20 06:47:49 +0300

    with images

--- src/help/v1.js
+++ src/help/v1.js
@@ -7,14 +7,19 @@ await page.goto('https://microservicesportchooser.azurewebsites.net/static/mpcv1
 
 await page.setViewportSize({ width: 1500, height: 889 })
 
-await page.click('input[name="appName"]')
-await page.type('input[name="appName"]', 'MyMicroserviceName')
+const input = await page.$('input[name="appName"]');
+// await page.click('input[name="appName"]')
+// await page.type('input[name="appName"]', 'MyMicroserviceName')
+var parentInput = await input.$('xpath=..');
+await parentInput.screenshot({ path: "microservice_name.png"});
+await input.type('MyMicroserviceName')
 
 const buttonDeter = await page.$("//button[contains(., 'Static')]");
+await buttonDeter.screenshot( { path: "generate_port.png"});
 await buttonDeter.click();
 // const element1 = await page.$('.mat-typography > app-root > app-primary-navig > .mat-drawer-container > .mat-drawer-content')
 await page.screenshot({ path: 'staticV1.png' })
 
-// await browser.close();
+ await browser.close();
 
 })();
