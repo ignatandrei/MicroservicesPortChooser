cde6fe244e88c682f0c04eaaa53ed000506ada9e
ignatandrei <ignatandrei@yahoo.com>
2021-08-20 07:50:24 +0300

    with intro from shepherd

--- src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
+++ src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , AfterViewInit } from '@angular/core';
 import { MPCService } from '../services/mpcv1.service';
-
+import { ShepherdService } from 'angular-shepherd';
 @Component({
   selector: 'app-mpcv1',
   templateUrl: './mpcv1.component.html',
   styleUrls: ['./mpcv1.component.css']
 })
-export class MPCv1Component implements OnInit {
+export class MPCv1Component implements OnInit , AfterViewInit {
 
   appName: string = '';
   pcName: string=  'localhost';
   fullName: string = '';
-  constructor(private MPCService: MPCService) { }
+  constructor(private MPCService: MPCService, private shepherdService: ShepherdService) { }
 
   ngOnInit(): void {
 
@@ -32,6 +32,84 @@ export class MPCv1Component implements OnInit {
     this.MPCService.nonDeterministic(this.appName).subscribe(
       v=> window.alert(this.constructFullName(v))
     );
+  }
+
+  ngAfterViewInit() {
+    var buttons=[
+      {
+        classes: 'shepherd-button-secondary',
+        text: 'Exit',
+        type: 'cancel'
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Back',
+        type: 'back'
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Next',
+        type: 'next'
+      }
+    ];
+
+    this.shepherdService.defaultStepOptions = {
+      
+      cancelIcon: {
+        enabled: true
+      },
+      classes: 'custom-class-name-1 custom-class-name-2',
+      highlightClass: 'highlight',
+      scrollTo: false,    
+      title: 'Welcome to Port Chooser v1!',
+     };
+     this.shepherdService.modal = true;
+     this.shepherdService.confirmCancel = false;
+     this.shepherdService.requiredElements = [
+      {
+        selector: '.first-element-help',
+        message: 'No found intro.',
+        title: 'MPCV1'
+      },
+      {
+        selector: '.formAppName-help',
+        message: 'waiting for app name',
+        title: 'MPCV1'
+      },
+    ];
+     this.shepherdService.addSteps(
+      [{
+        id: 'intro',
+        attachTo: { 
+          element: '.first-element-help', 
+          on: 'bottom'
+        },
+        text: ['Click next for tour or exit if you know how to use already the app'],
+        buttons: buttons,
+       },
+
+      {
+        id: 'nameApp',
+        attachTo: { 
+          element: '.formAppName-help', 
+          on: 'right'
+        },
+        text: ['Please input here the name of the microservice'],
+        buttons: buttons,
+      },
+      {
+        id: 'generatePort',
+        attachTo: { 
+          element: '.generatePort-help', 
+          on: 'bottom'
+        },
+        text: ['And click here to generate static port from name'],
+        buttons: buttons,
+      }
+
+    ]);
+    // this.shepherdService.addSteps(defaultSteps);
+     this.shepherdService.start();
   }
 
 }
