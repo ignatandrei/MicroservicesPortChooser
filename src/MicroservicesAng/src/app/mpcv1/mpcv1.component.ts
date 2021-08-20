96a66657f6182616e6124dfe28e31a9a0b4a45b1
ignatandrei <ignatandrei@yahoo.com>
2021-08-20 08:46:43 +0300

    added help

--- src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
+++ src/MicroservicesAng/src/app/mpcv1/mpcv1.component.ts
@@ -1,115 +1,119 @@
-import { Component, OnInit , AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MPCService } from '../services/mpcv1.service';
 import { ShepherdService } from 'angular-shepherd';
 @Component({
   selector: 'app-mpcv1',
   templateUrl: './mpcv1.component.html',
-  styleUrls: ['./mpcv1.component.css']
+  styleUrls: ['./mpcv1.component.css'],
 })
-export class MPCv1Component implements OnInit , AfterViewInit {
-
+export class MPCv1Component implements OnInit, AfterViewInit {
   appName: string = '';
-  pcName: string=  'localhost';
+  pcName: string = 'localhost';
   fullName: string = '';
-  constructor(private MPCService: MPCService, private shepherdService: ShepherdService) { }
+  constructor(
+    private MPCService: MPCService,
+    private shepherdService: ShepherdService
+  ) {}
 
-  ngOnInit(): void {
-
-  }
-  constructFullName(port:string): string{
-    this.fullName = 'http://'+ (this.pcName ||'localhost') + ':' + port;
+  ngOnInit(): void {}
+  constructFullName(port: string): string {
+    this.fullName = 'http://' + (this.pcName || 'localhost') + ':' + port;
     return this.fullName;
-  } 
-  
-  onDeterministic(): void {
+  }
 
-     this.MPCService.deterministic(this.appName).subscribe(
-       v=> window.alert(this.constructFullName(v))
-     );
+  onDeterministic(): void {
+    this.MPCService.deterministic(this.appName).subscribe((v) =>
+      window.alert(this.constructFullName(v))
+    );
   }
 
   onNonDeterministic(): void {
-    this.MPCService.nonDeterministic(this.appName).subscribe(
-      v=> window.alert(this.constructFullName(v))
+    this.MPCService.nonDeterministic(this.appName).subscribe((v) =>
+      window.alert(this.constructFullName(v))
     );
   }
 
   ngAfterViewInit() {
-    var buttons=[
+    var buttons = [
       {
         classes: 'shepherd-button-secondary',
         text: 'Exit',
-        type: 'cancel'
+        type: 'cancel',
       },
       {
         classes: 'shepherd-button-primary',
         text: 'Back',
-        type: 'back'
+        type: 'back',
       },
       {
         classes: 'shepherd-button-primary',
         text: 'Next',
-        type: 'next'
-      }
+        type: 'next',
+      },
     ];
 
     this.shepherdService.defaultStepOptions = {
-      
       cancelIcon: {
-        enabled: true
+        enabled: true,
       },
+      modal: true,
       classes: 'custom-class-name-1 custom-class-name-2',
       highlightClass: 'highlight',
-      scrollTo: false,    
+      scrollTo: false,
       title: 'Welcome to Port Chooser v1!',
-     };
-     this.shepherdService.modal = true;
-     this.shepherdService.confirmCancel = false;
-     this.shepherdService.requiredElements = [
+    };
+    this.shepherdService.modal = true;
+    this.shepherdService.confirmCancel = false;
+    this.shepherdService.requiredElements = [
       {
         selector: '.first-element-help',
         message: 'No found intro.',
-        title: 'MPCV1'
+        title: 'MPCV1',
       },
       {
         selector: '.formAppName-help',
         message: 'waiting for app name',
-        title: 'MPCV1'
+        title: 'MPCV1',
       },
     ];
-     this.shepherdService.addSteps(
-      [{
+    //     this.shepherdService.steps = [];
+    // this.shepherdService.tourObject.steps = [];
+    this.shepherdService.modal = true;
+    this.shepherdService.addSteps([
+      {
         id: 'intro',
-        attachTo: { 
-          element: '.first-element-help', 
-          on: 'bottom'
+        attachTo: {
+          element: '.first-element-help',
+          on: 'bottom',
         },
-        text: ['Click next for tour or exit if you know how to use already the app'],
+        text: [
+          'Click next for tour or exit if you know how to use already the app',
+        ],
         buttons: buttons,
-       },
+      },
 
       {
         id: 'nameApp',
-        attachTo: { 
-          element: '.formAppName-help', 
-          on: 'right'
+        attachTo: {
+          element: '.formAppName-help',
+          on: 'right',
         },
         text: ['Please input here the name of the microservice'],
         buttons: buttons,
       },
       {
         id: 'generatePort',
-        attachTo: { 
-          element: '.generatePort-help', 
-          on: 'bottom'
+        attachTo: {
+          element: '.generatePort-help',
+          on: 'bottom',
         },
         text: ['And click here to generate static port from name'],
         buttons: buttons,
-      }
-
+      },
     ]);
     // this.shepherdService.addSteps(defaultSteps);
-     this.shepherdService.start();
   }
-
+  onHelp(): void {
+    this.shepherdService.show('intro');
+  }
 }
--- src/MicroservicesAng/src/app/mpcv2/mpcv2.component.ts
+++ src/MicroservicesAng/src/app/mpcv2/mpcv2.component.ts
@@ -1,32 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ShepherdService } from 'angular-shepherd';
 import { MPCService } from '../services/mpcv1.service';
 
 @Component({
   selector: 'app-mpcv2',
   templateUrl: './mpcv2.component.html',
-  styleUrls: ['./mpcv2.component.css']
+  styleUrls: ['./mpcv2.component.css'],
 })
-export class Mpcv2Component implements OnInit {
-
+export class Mpcv2Component implements OnInit, AfterViewInit {
   appName: string = '';
-  pcName: string=  'localhost';
+  pcName: string = 'localhost';
   tag: string = '';
   fullName: string = '';
-  constructor(private MPCService: MPCService) { }
+  constructor(
+    private MPCService: MPCService,
+    private shepherdService: ShepherdService
+  ) {}
+  ngAfterViewInit() {
+    var buttons = [
+      {
+        classes: 'shepherd-button-secondary',
+        text: 'Exit',
+        type: 'cancel',
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Back',
+        type: 'back',
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Next',
+        type: 'next',
+      },
+    ];
 
-  ngOnInit(): void {
+    this.shepherdService.steps = [];
+    if (this.shepherdService.tourObject) {
+      this.shepherdService.tourObject.steps = [];
+    }
+    this.shepherdService.modal = true;
+    this.shepherdService.defaultStepOptions = {
+      cancelIcon: {
+        enabled: true,
+      },
+      modal: true,
+      classes: 'custom-class-name-1 custom-class-name-2',
+      highlightClass: 'highlight',
+      scrollTo: false,
+      title: 'Welcome to Port Chooser v2!',
+    };
 
+    this.shepherdService.confirmCancel = false;
+    this.shepherdService.requiredElements = [
+      {
+        selector: '.first-element-help',
+        message: 'No found intro.',
+        title: 'MPCV2',
+      },
+      {
+        selector: '.formAppName-help',
+        message: 'waiting for app name',
+        title: 'MPCV2',
+      },
+    ];
+    this.shepherdService.addSteps([
+      {
+        id: 'intro',
+        attachTo: {
+          element: '.first-element-help',
+          on: 'bottom',
+        },
+        text: [
+          'Click next for tour or exit if you know how to use already the app',
+        ],
+        buttons: buttons,
+      },
+
+      {
+        id: 'nameApp',
+        attachTo: {
+          element: '.formAppName-help',
+          on: 'right',
+        },
+        text: ['Please input here the name of the microservice'],
+        buttons: buttons,
+      },
+      {
+        id: 'nameTag',
+        attachTo: {
+          element: '.formTag-help',
+          on: 'right',
+        },
+        text: ['Please input here the tag of the microservice -can be a different folder'],
+        buttons: buttons,
+      },
+      {
+        id: 'generatePort',
+        attachTo: {
+          element: '.generatePort-help',
+          on: 'bottom',
+        },
+        text: ['And click here to generate static port from name'],
+        buttons: buttons,
+      },
+    ]);
+    // this.shepherdService.addSteps(defaultSteps);
   }
-  constructFullName(port:string): string{
-    this.fullName = 'http://'+ (this.pcName ||'localhost') + ':' + port;
+  ngOnInit(): void {}
+  constructFullName(port: string): string {
+    this.fullName = 'http://' + (this.pcName || 'localhost') + ':' + port;
     return this.fullName;
-  } 
-  
-  onDeterministic(): void {
-
-     this.MPCService.deterministicV2(this.appName,this.tag).subscribe(
-       v=> window.alert(this.constructFullName(v))
-     );
   }
-
+  onHelp(): void {
+    this.shepherdService.show('intro');
+  }
+  onDeterministic(): void {
+    this.MPCService.deterministicV2(this.appName, this.tag).subscribe((v) =>
+      window.alert(this.constructFullName(v))
+    );
+  }
 }
