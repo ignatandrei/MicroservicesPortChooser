f9922d81894f8760eeb8ef687d274c82d3736ac1
ignatandrei <ignatandrei@yahoo.com>
2021-08-23 11:20:40 +0300

    component

--- src/MicroservicesAng/src/app/registered-mpc/registered-mpc.component.ts
+++ src/MicroservicesAng/src/app/registered-mpc/registered-mpc.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Register } from '../classes/register';
 import { MPCService } from '../services/mpcv1.service';
-
+import {Sort} from '@angular/material/sort';
 @Component({
   selector: 'app-registered-mpc',
   templateUrl: './registered-mpc.component.html',
@@ -17,4 +17,29 @@ export class RegisteredMPCComponent implements OnInit {
       data => this.data = data
     );
   }
+  compare(a: number | string |Date, b: number | string|Date, isAsc: boolean) : number {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  sortData(sort: Sort) {
+    const data1 = this.data.slice();
+    if (!sort.active || sort.direction === '') {
+      this.data = data1;
+      return;
+    }
+
+    this.data = data1.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'uniqueID': return this.compare(a.uniqueID, b.uniqueID, isAsc);
+        case 'dateRegistered': return this.compare(a.dateRegistered, b.dateRegistered, isAsc);
+        case 'tag': return this.compare(a.tag, b.tag, isAsc);
+        default: return 0;
+      }
+    });
+  }
 }
+function sort(sort: any, Sort: any) {
+  throw new Error('Function not implemented.');
+}
+
