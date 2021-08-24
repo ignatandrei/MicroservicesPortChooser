bcc0d0f211b03c664da22f03da0ec73a5c363270
ignatandrei <ignatandrei@yahoo.com>
2021-08-24 11:25:11 +0300

    auto register himself

--- src/MicroservicesPortChooser/MSPCWebExtension/DiscoveryAndRegister.cs
+++ src/MicroservicesPortChooser/MSPCWebExtension/DiscoveryAndRegister.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using MicroservicesPortChooserBL;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -27,7 +29,17 @@ namespace MSPCWebExtension
                 var add = s?.Features?.Get<IServerAddressesFeature>()?.Addresses?.ToArray();
                 if ((add?.Length ??0) > 0)
                 {
-                    Console.WriteLine("not null!!!" + string.Join(",", add));
+                    await Task.Delay(5000, stoppingToken);
+                    using var h = new HttpClient();
+                    h.BaseAddress = new Uri("https://microservicesportchooser.azurewebsites.net/api/v1/");
+                    var p = new PortService(h);
+                    foreach (var item in add) {
+                        var u = new Uri(item);
+                        var r = new Register("andrei",u.Host, (UInt16)u.Port,"tag");
+                        await p.AddNew(r);
+                    }
+                    
+                    //Console.WriteLine("not null!!!" + string.Join(",", add));
                     return;
                 }
                 else
---
+++ src/MicroservicesPortChooser/MSPCWebExtension/PortService.cs
@@ -0,0 +1,13 @@
+﻿using MicroservicesPortChooserBL;
+using HttpClientGenerator.Shared;
+using System.Threading.Tasks;
+
+namespace MSPCWebExtension
+{
+    public partial class PortService
+    {
+        [HttpPost("Register/AddNew")]
+        public partial Task<Register> AddNew(Register r);
+       
+    }
+}
