afb6933b12e9a0d5e46c9a12eaddaac36a1edb2d
ignatandrei <ignatandrei@yahoo.com>
2021-08-29 08:48:35 +0300

    working with IIS

--- src/MicroservicesPortChooser/MSPCWebExtension/DiscoveryAndRegister.cs
+++ src/MicroservicesPortChooser/MSPCWebExtension/DiscoveryAndRegister.cs
@@ -25,8 +25,16 @@ namespace MSPCWebExtension
             this.config = config;
             this.serviceProvider = serviceProvider;
         }
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        private async Task<bool> RegisterMe(IServer s, CancellationToken stoppingToken)
         {
+            if (s == null)
+                return false;
+            var add = s?.Features?.Get<IServerAddressesFeature>()?.Addresses?.ToArray();
+            var nrAddresses = add?.Length ?? 0;
+            Console.WriteLine($"nr adresses {nrAddresses}");
+            if (nrAddresses == 0)
+                return false;
+
             var configData = new ConfigDataMSPC();
             config.GetSection("MSPC").Bind(configData);
             if (string.IsNullOrWhiteSpace(configData.appName))
@@ -34,43 +42,56 @@ namespace MSPCWebExtension
             if (string.IsNullOrWhiteSpace(configData.registerUrl))
                 configData.appName = "https://microservicesportchooser.azurewebsites.net/api/v1/";
 
+            await Task.Delay(5000, stoppingToken);
+            using var h = new HttpClient();
+            h.BaseAddress = new Uri(configData.registerUrl);
+            var p = new PortService(h);
+            foreach (var addres in add)
+            {
+                var url = addres;
+                if (url.Contains("*"))
+                    url = url.Replace("*", Environment.MachineName);
+
+                Console.WriteLine($"try to register {url}");
+                var u = new Uri(url);
+                string host = u.Host;
+                if (host == "[::]" || host == "0.0.0.0")
+                    host = Environment.MachineName;
+
+                var r = new Register(configData.appName, host, u.Port, configData.tag, u.Authority);
+                r.PCName = Environment.MachineName;
+                try
+                {
+                    await p.AddNew(r);
+                    Console.WriteLine($"done register {url}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"error in register {url} : {ex.Message}");
+                    return false;
+                }
+            }
+            return true;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 var s = serviceProvider.GetService(typeof(IServer)) as IServer;
-                var add = s?.Features?.Get<IServerAddressesFeature>()?.Addresses?.ToArray();
-                if ((add?.Length ??0) > 0)
-                {
-                    await Task.Delay(5000, stoppingToken);
-                    using var h = new HttpClient();
-                    h.BaseAddress = new Uri(configData.registerUrl);
-                    var p = new PortService(h);
-                    foreach (var item in add) {
-                        var u = new Uri(item);
-                        string host = u.Host;
-                        if (host == "[::]" || host=="0.0.0.0")
-                            host = Environment.MachineName;
 
-                        var r = new Register(configData.appName, host, u.Port, configData.tag, u.Authority);
-                        r.PCName = Environment.MachineName;
-                        try
-                        {
-                            await p.AddNew(r);
-                            Console.WriteLine($"done register {item}");
-                        }
-                        catch(Exception ex)
-                        {
-                            Console.WriteLine("error register: " + ex.Message);
-                        }
-                    }
-                    
-                    //Console.WriteLine("not null!!!" + string.Join(",", add));
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("waiting");
-                }
+                Console.WriteLine($"waiting for IServer : {s == null}");               
                 await Task.Delay(5000, stoppingToken);
+                try
+                {
+                    var success= await RegisterMe(s, stoppingToken);
+                    if (success)
+                        return;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"error in register : {ex.Message}");
+                }
 
             }
         }
