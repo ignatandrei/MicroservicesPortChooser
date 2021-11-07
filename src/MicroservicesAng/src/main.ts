042d6efc3069d907aa1aada0a38e3db033c28553
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-07 04:32:45 +0200

    try again open replay

--- src/MicroservicesAng/src/main.ts
+++ src/MicroservicesAng/src/main.ts
@@ -3,10 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Tracker from '@openreplay/tracker';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+.then(() => {
+  const tracker = new Tracker({
+    projectKey: "kbcFbNn6mWwBgtMGIn2q",  
+  });
+    tracker.start();
+})
   .catch(err => console.error(err));
