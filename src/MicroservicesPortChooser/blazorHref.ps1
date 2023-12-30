911b2a0a28cf10117839609ca726dd5f72138528
Andrei Ignat <ignatandrei@yahoo.com>
2023-12-30 18:34:56 +0200

    Update .gitignore and dotnet-tools.json, delete blazorHref.ps1 script

--- src/MicroservicesPortChooser/blazorHref.ps1
+++ src/MicroservicesPortChooser/blazorHref.ps1
@@ -6,11 +6,11 @@ param(
      [string]$newRelativePath ="/blazor/"
 
  )
-cls
+#cls
 # $file = "index.html"
 Write-Host "start reading " $file
 $FileContents = Get-Content -Path $file
-
+$newFileContents = @()
 $i = 1
 ForEach ($Line in $FileContents) {
     # Process each line here
@@ -18,21 +18,36 @@ ForEach ($Line in $FileContents) {
 if ($Line -like '* href="/*') { 
     $Line=$Line.Replace(' href="/',' href="'+$newRelativePath)
     Write-Host "with / Line# $i :" $Line
+    $newFileContents += $Line
     continue  
 }
 
 if ($Line -like '* href="*') { 
     $Line=$Line.Replace(' href="',' href="'+$newRelativePath)
     Write-Host "no / Line# $i :" $Line
+    $newFileContents += $Line
+    continue
   
 }
 if ($Line -like '* src="*') { 
     $Line=$Line.Replace(' src="',' src="'+$newRelativePath)
     Write-Host "no / Line# $i :" $Line
-  
+    $newFileContents += $Line
+    continue
 }
+
+if ($Line -like "*'service*") { 
+    $Line=$Line.Replace("'service","'"+ $newRelativePath + "service" )
+    Write-Host "no / Line# $i :" $Line
+    $newFileContents += $Line
+    continue
+}
+
+    $newFileContents += $Line
 
     $i++
 }
 
 Write-Host "End " $file
+
+$newFileContents  | Set-Content -Path $file
