0542014422f3b00ec8092285720f79ceefe4d561
Andrei Ignat <ignatandrei@yahoo.com>
2024-01-23 20:24:42 +0200

    rtesting

---
+++ src/MicroservicesPortChooser/Generator/MSPC_CRUD20231225/MSPC_CRUD20231225/MSPC_CRUD20231225.Controllers/Generated/ApplicationDBContext/test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Generated;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class testController: Controller
+{
+    private readonly ApplicationDBContext applicationDBContext;
+
+    public testController(ApplicationDBContext applicationDBContext)
+    {
+        this.applicationDBContext = applicationDBContext;
+    }
+    public string test1()
+    {
+        return "test1";
+    }
+    public string test2()
+    {
+        return applicationDBContext.ContextId.InstanceId.ToString();
+    }
+    public string test3()
+    {
+        return applicationDBContext.Database?.GetConnectionString()??"NO connection string";
+    }
+}
